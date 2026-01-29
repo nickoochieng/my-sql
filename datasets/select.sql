df5885bc41ab3129ba595ff5413505fab673f4cd
nickoochieng <1nickochieng@gmail.com>
2026-01-29 23:41:15 +0300

    more updates

--- datasets/select.sql
+++ datasets/select.sql
@@ -7,3 +7,16 @@ FROM employees;
 
 SELECT first_name, last_name, hire_date
 FROM employees;
+
+-- Where Clause is used when we are looking for a specific thing.
+
+SELECT *
+FROM employees WHERE employee_id = 4;
+
+SELECT *
+FROM employees WHERE first_name = 'Spongebob';
+
+SELECT *
+FROM employees WHERE hourly_pay >= 15.00;
+
+
---
+++ datasets/update.sql
@@ -0,0 +1,8 @@
+UPDATE employees
+SET hourly_pay = 10.50,
+	hire_date = '2010-09-07'
+WHERE first_name = 'Nick';
+
+SELECT * FROM employees;
+
+SET SQL_SAFE_UPDATES = 0;
